ff2e3eb7bc1440a52ef43f7d8ee82140cede5ab7
Kanako-muff <kanako.design00@gmail.com>
2024-12-30 02:03:41 -0700

    Initial commit. home/service done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/home/Home";
+import Services from "./components/services/Services";
+import Skills from "./components/skills/Skills";
+
+function App() {
+  return (
+    <main className="main">
+      <Home />
+      <Services />
+      <Skills />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="section__title text-cs">Skills</h2>
+      <p className="section__subtitle">
+        My <span>Talent</span>
+      </p>
+    </section>
+  );
+};
+
+export default Skills;
