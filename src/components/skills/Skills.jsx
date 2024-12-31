ef5b513f7d31173bc30ee7c8e089e528d054e1f7
Kanako-muff <kanako.design00@gmail.com>
2024-12-31 02:24:20 -0700

    skills done

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { skills } from "../../Data";
+import "./skills.css";
 
 const Skills = () => {
   return (
@@ -7,6 +9,30 @@ const Skills = () => {
       <p className="section__subtitle">
         My <span>Talent</span>
       </p>
+
+      <div className="skills__container container grid">
+        {skills.map(({ name, percentage, description }, inbox) => {
+          return (
+            <div className="skills__item">
+              <div className="skills__titles">
+                <h3 className="skills__name">{name}</h3>
+                <span className="skills__number">
+                  {percentage} <span>%</span>
+                </span>
+              </div>
+              <p className="skills__description">{description}</p>
+              <div className="skills__bar">
+                <span
+                  className="skills__percentage"
+                  style={{ width: `${percentage}%` }}
+                >
+                  <span></span>
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
