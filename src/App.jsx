8d5e47367eccb8ce8ff1d3d44910cf0a9377a817
Kanako-muff <kanako.design00@gmail.com>
2025-01-12 15:06:44 -0700

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Resume from "./components/resume/Resume";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <main className="main">
+      <Header />
       <Home />
       <Services />
       <Skills />
